eeb321d25d321d5614b5b0a87544c566e31764ab
UiPathSerban <80847945+UiPathSerban@users.noreply.github.com>
2023-10-12 10:31:17 +0300

    Small changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    uiBankApiUrl: 'http://localhost:8080/api'
+    // uiBankApiUrl: 'http://localhost:8080/api'
+    uiBankApiUrl: 'https://uibank-api.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    uiBankApiUrl: 'http://localhost:8080/api'
+    // uiBankApiUrl: 'http://localhost:8080/api'
+    uiBankApiUrl: 'https://uibank-api.azurewebsites.net/api'
+
 };
